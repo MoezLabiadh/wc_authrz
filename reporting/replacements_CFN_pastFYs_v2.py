05981a47db5d57e6ed4864e957657f6c8dbf08bd
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2021-12-13 08:38:49 -0800

    Update and rename replacements_CFN_pastFYs_v2.py to reporting/replacements_CFN_pastFYs_v2.py

--- reporting/replacements_CFN_pastFYs_v2.py
+++ reporting/replacements_CFN_pastFYs_v2.py
@@ -185,8 +185,6 @@ def main():
 
     '''
     print ('Connecting to BCGW...PLease enter your credentials')
-    bcgw_user_name = 'MLABIADH'
-    bcgw_password = 'MoezLab8814'
     #bcgw_user_name = raw_input("Enter your BCGW username:")
     #bcgw_password = raw_input("Enter your BCGW password:")
     bcgw_conn_path = create_bcgw_connection(workspace, bcgw_user_name,bcgw_password)
