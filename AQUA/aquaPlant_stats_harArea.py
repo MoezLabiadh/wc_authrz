432552c013a6ecd21151f2aa54ddfce9ab962681
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-04-19 10:05:52 -0700

    Update aquaPlant_stats_harArea.py

--- AQUA/aquaPlant_stats_harArea.py
+++ AQUA/aquaPlant_stats_harArea.py
@@ -22,8 +22,8 @@ df = df.sort_values(by=['Appl_num'])
 df['Species_Group'] = df['Species_Group'].astype(int).astype(str)
 df['Appl_num'] = df['Appl_num'].astype(str)
 
-#har_areas = df['Harvest_Area_Num'].unique()
-har_areas = ['5107']
+har_areas = df['Harvest_Area_Num'].unique()
+#har_areas = ['5107','5405', '5438']
 
 for ha in har_areas:
     print ('working on Harvest Area {} '.format(ha))
@@ -42,39 +42,50 @@ for ha in har_areas:
                        value_vars=['Quota Requested', 'Quota Approved' ], ignore_index=True)
     
     
-    
     sp_grps = list(df_ha['Species_Group'].unique())
     
     # initiate the figure/plot
-    fig, ax = plt.subplots(nrows=len(sp_grps), figsize=(20,len(sp_grps)*6))
-    t_txt = 'Wild Aquatic Plant Harvest by Year\n Harvest Area # {}'.format (ha)  
-    fig.suptitle(t_txt, fontsize=30)
-    #plt.tight_layout()
+
+ 
+    if len(sp_grps) in (1,2):
+        h_coef = 6
+    elif len(sp_grps) == 3:
+        h_coef = 5
+    else:
+        h_coef = 4
+
+    fig, axes = plt.subplots(nrows=len(sp_grps), figsize=(16,len(sp_grps)*h_coef), constrained_layout=True)
+    t_txt = 'Wild Aquatic Plant Harvest by Year (#{})'.format (ha)  
+    fig.suptitle(t_txt, fontsize=20)
+    #fig.tight_layout()
     
     #Create one subplot for each Species Group
     for i, sp_g in enumerate(sp_grps):
         filt_hr = df_hr.loc [df_hr['Species_Group'] == sp_g].reset_index()
         filt_qt= df_qt.loc [df_qt['Species_Group'] == sp_g]
    
-        
+        if len(sp_grps) == 1:
+            ax = axes
+        else:
+            ax = axes[i]
+            
         sns.barplot(data = filt_qt, x='Year', y='value', alpha=0.7,
-                    palette = ['tab:orange', 'tab:blue'], hue = 'variable', ax=ax[i])
+                    palette = ['tab:orange', 'tab:blue'], hue = 'variable', ax=ax)
         
         sns.lineplot(data =filt_hr['Total_Quantity_harvested'], linewidth = 1.5, markers=True,sort = False,
-                     marker="o",markersize=10, color='darkred', label='Harvested Quantity', ax=ax[i])   
+                     marker="o",markersize=10, color='darkred', label='Harvested Quantity', ax=ax)   
         
         
         #Set labels
-        ax[i].set_title('Species Group: {}'.format(sp_g), size=25)
-        ax[i].set_ylabel('Quantity (tonne)', fontsize=14)
+        ax.set_title('Species Group: {}'.format(sp_g), size=15)
+        ax.set_ylabel('Quantity (tonne)', fontsize=14)
         
         if sp_g == sp_grps[-1]:
-            ax[i].set_xlabel('Harvest Year', fontsize=14)
+            ax.set_xlabel('Harvest Year', fontsize=14)
         else:
-            ax[i].set_xlabel(xlabel = None)
+            ax.set_xlabel(xlabel = None)
             
-
-        
+  
         #label bars
         #for container in ax1.containers:
             #ax1.bar_label(container)
@@ -84,8 +95,14 @@ for ha in har_areas:
         
         #Remove legend
         #ax1.legend(title='Legend')
-        handles, labels = ax[i].get_legend_handles_labels()
-        ax[i].legend(handles=handles, labels=labels)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles=handles, labels=labels)
         
-    filename = 'graph_harvArea{}.png'.format (ha)
-    fig.savefig(os.path.join(wks, 'outputs', 'plots', filename))
+    filename = 'graph_harvArea_{}.png'.format (ha)
+    fig.savefig(os.path.join(wks, 'outputs', 'plots', 'by_harvest_area', filename))
+
+
+
+
+
+
