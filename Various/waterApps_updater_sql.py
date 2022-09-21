a4b94ad8cfce8c11af6ecf725026db4cf2d8a939
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-09-21 15:22:31 -0700

    Update waterApps_updater_sql.py

--- Various/waterApps_updater_sql.py
+++ Various/waterApps_updater_sql.py
@@ -4,6 +4,7 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 
+
 def connect_to_DB (username,password,hostname):
     """ Returns a connection to Oracle database"""
     try:
@@ -29,7 +30,7 @@ def read_query(connection,query):
             cursor.close()
 
 
-def update_wtshd_info(f, connection, sql):
+def update_wtshd_info(f, connection, sql, df_lk):
     """Updates the Watershed and Region info of the Water Apps ledger"""
     wb = load_workbook(f)
     ws = wb['Active Applications']
@@ -37,7 +38,7 @@ def update_wtshd_info(f, connection, sql):
     for i, row in enumerate(ws.iter_rows(min_row=2,values_only=True)):
         row_id = i+2
         if ws['X{}'.format(row_id)].value is None:
-            print ("...working on Row {}".format (row_id))
+            print ("..working on Row {}".format (row_id))
             lat = row[10]
             long = row[11]
             
@@ -52,6 +53,10 @@ def update_wtshd_info(f, connection, sql):
                 wtrsh_name = df_sql['WATER_LICENSING_WATERSHED_NAME'].iloc[0]
                 ws['X{}'.format(row_id)] = wtrsh_name
                 ws['Y{}'.format(row_id)]  = """=VLOOKUP(X{},'Pick Lists'!$L$1:$M$107,2,FALSE)""".format(row_id)
+                
+                df_lk_i = df_lk.loc[df_lk['WATER_LICENSING_WATERSHED'] == wtrsh_name]
+                print ('....Watershade is {}'.format(wtrsh_name))
+                print ('....Region is {}'.format(df_lk_i['REGION'].iloc[0]))
             
         else:
             pass
@@ -67,28 +72,30 @@ def update_wtshd_info(f, connection, sql):
     
     wb.save(f)
     
-def main():
-    workspace = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20220916_waterLicencing_support'
-    f = os.path.join (workspace,'Water Application Ledger_workingCopy.xlsx')
+#def main():
+workspace = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20220916_waterLicencing_support'
+f = os.path.join (workspace,'Water Application Ledger_workingCopy.xlsx')
+df_lk = pd.read_excel(f, 'Pick Lists')
+df_lk = df_lk[['WATER_LICENSING_WATERSHED', 'REGION']]
+
+print ('Connecting to BCGW...')
+hostname = 'bcgw.bcgov/idwprod1.bcgov'
+bcgw_user = os.getenv('bcgw_user')
+bcgw_pwd = os.getenv('bcgw_pwd')
+connection = connect_to_DB (bcgw_user,bcgw_pwd,hostname)
+
+sql = '''
+        SELECT 
+            WATER_LICENSING_WATERSHED_NAME
+        FROM 
+            WHSE_WATER_MANAGEMENT.WLS_WATER_LIC_WATERSHEDS_SP wsh
+        WHERE 
+            SDO_RELATE (wsh.SHAPE, 
+                        SDO_GEOMETRY('POINT({long} {lat})', 4326),
+                        'mask=ANYINTERACT') = 'TRUE'
+    '''
     
-    print ('Connecting to BCGW...')
-    hostname = 'bcgw.bcgov/idwprod1.bcgov'
-    bcgw_user = os.getenv('bcgw_user')
-    bcgw_pwd = os.getenv('bcgw_pwd')
-    connection = connect_to_DB (bcgw_user,bcgw_pwd,hostname)
+print ("Updating Watershed and Region info...")
+update_wtshd_info(f, connection, sql, df_lk)
     
-    sql = '''
-            SELECT 
-                WATER_LICENSING_WATERSHED_NAME
-            FROM 
-                WHSE_WATER_MANAGEMENT.WLS_WATER_LIC_WATERSHEDS_SP wsh
-            WHERE 
-                SDO_RELATE (wsh.SHAPE, 
-                            SDO_GEOMETRY('POINT({long} {lat})', 4326),
-                            'mask=ANYINTERACT') = 'TRUE'
-        '''
-        
-    print ("Updating Watershed and Region info...")
-    update_wtshd_info(f, connection, sql)
-    
-main()
+#main()
