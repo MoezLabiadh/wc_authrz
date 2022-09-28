2c49ef3b5864246ba51f16bd62be20a083d796a8
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-09-28 15:16:29 -0700

    Update waterApps_updater_arcpy.py

--- Various/waterApps_updater_arcpy.py
+++ Various/waterApps_updater_arcpy.py
@@ -5,20 +5,31 @@ from openpyxl import load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 
 
-def update_wtshd_info(f, wtsh_fc,df_lk):
+def update_wtshd_info(f, wtsh_fc,df_lk,saveOp):
     """Updates the Watershed and Region info of the Water Apps ledger"""
     wb = load_workbook(f)
     ws = wb['Active Applications']
 
     for i, row in enumerate(ws.iter_rows(min_row=2,values_only=True)):
         row_id = i+2
+
         if ws['X{}'.format(row_id)].value is None:
-            print ("..working on Row {}".format (row_id))
+            print ("\n********Working on Row {}********".format (row_id))
             lat = row[10]
             long = row[11]
 
+            print ('Latitude: {}'. format(lat))
+            print ('Longitude: {}'. format(long))
+
             if (lat is None) or (long is None):
-                raise Exception("ERROR: Latitude or/and Longitude values are not specified!")
+                raise Exception("Latitude or/and Longitude values are not specified!")
+
+            elif not min(54.465, 48.2) < lat < max(54.5, 48.134):
+                raise Exception("Latitude value is out of range!")
+
+
+            elif not min(-122.634, -133.257) < long < max(-122.7, -133.3):
+                    raise Exception("Longitude value is out of range!")
 
             else:
                 pt = arcpy.Point(float(long),float(lat))
@@ -39,14 +50,13 @@ def update_wtshd_info(f, wtsh_fc,df_lk):
                 ws['Y{}'.format(row_id)]  = """=VLOOKUP(X{},'Pick Lists'!$L$1:$M$107,2,FALSE)""".format(row_id)
 
                 df_lk_i = df_lk.loc[df_lk['WATER_LICENSING_WATERSHED'] == wtrsh_name]
-                print ('....Watershade is {}'.format(wtrsh_name))
-                print ('....Region is {}'.format(df_lk_i['REGION'].iloc[0]))
-
-                wb.save(f)
+                print ('Watershed: {}'.format(wtrsh_name))
+                print ('Sub-region: {}'.format(df_lk_i['REGION'].iloc[0]))
 
                 arcpy.Delete_management('in_memory')
 
         else:
+            #raise Exception ('No Empty watershed values found in spreadsheet')
             pass
 
     #Add data validation for Watershed name.
@@ -58,17 +68,25 @@ def update_wtshd_info(f, wtsh_fc,df_lk):
     dv.add("X2:X" + str(ws.max_row))
     ws.add_data_validation(dv)
 
-    wb.save(f)
+    if saveOp == 'Yes':
+        print('Writing Watershed and Subregion info to the Spreadsheet')
+        wb.save(f)
+
+    else:
+        pass
 
 def main():
     workspace = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20220916_waterLicencing_support'
     f = os.path.join (workspace,'Water Application Ledger_workingCopy2.xlsx')
     wtsh_fc = os.path.join (workspace,'data.gdb', 'watersheds_WC_all')
 
+    saveOp = 'No' # yes or no
+
+
     df_lk = pd.read_excel(f, 'Pick Lists')
     df_lk = df_lk[['WATER_LICENSING_WATERSHED', 'REGION']]
 
-    print ("Updating Watershed and Region info...")
-    update_wtshd_info(f, wtsh_fc,df_lk)
+    print ("Retrieving Watershed and Sub-region info...")
+    update_wtshd_info(f, wtsh_fc,df_lk,saveOp)
 
 main()
