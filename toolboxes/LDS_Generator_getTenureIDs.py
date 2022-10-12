95e9113698268df416ff9273c000a566a489f01d
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-10-12 10:47:25 -0700

    Update LDS_Generator_getTenureIDs.py

--- toolboxes/LDS_Generator_getTenureIDs.py
+++ toolboxes/LDS_Generator_getTenureIDs.py
@@ -1,5 +1,7 @@
+import os
 import cx_Oracle
 import pandas as pd
+
 
 def connect_to_DB (username,password,hostname):
     """ Returns a connection to Oracle database"""
@@ -17,30 +19,32 @@ def read_query(connection,query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        names = [ x[0] for x in cursor.description]
+        names = [x[0] for x in cursor.description]
         rows = cursor.fetchall()
         return pd.DataFrame(rows, columns=names)
     
     finally:
         if cursor is not None:
             cursor.close()
+  
             
 def main ():
     print (" Connect to BCGW")    
-    hostname = 'bcgw.bcgov/idwprod1.bcgov'
-    username = 'XXXX'
-    password = 'XXXX'
-    connection = connect_to_DB (username,password,hostname)
+    bcgw_host = 'bcgw.bcgov/idwprod1.bcgov'
+    bcgw_user = os.getenv('bcgw_user')
+    bcgw_pwd = os.getenv('bcgw_pwd')
+    connection = connect_to_DB (bcgw_user,bcgw_pwd,bcgw_host)
     
     print ("Execute SQL Query")
     
     sql = """
-           SELECT CROWN_LANDS_FILE, DISPOSITION_TRANSACTION_SID, INTRID_SID, 
-                  TENURE_STAGE, TENURE_STATUS
+           SELECT
+               CROWN_LANDS_FILE, DISPOSITION_TRANSACTION_SID, INTRID_SID, 
+               TENURE_STAGE, TENURE_STATUS
                   
-           FROM WHSE_TANTALIS.TA_CROWN_TENURES_VW
+           FROM WHSE_TANTALIS.TA_CROWN_TENURES_SVW
      
-          WHERE CROWN_LANDS_FILE = '{t}'
+          WHERE CROWN_LANDS_FILE =  '{t}'
             """
     in_tenure_nbr =   input('Enter File Number:')
     query = sql.format(t = str(in_tenure_nbr).strip())
