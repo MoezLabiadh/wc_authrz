337863a97d386660f47dbc45bf2c3d2c27a8bbaa
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2023-01-23 14:04:46 -0800

    Update Maanluth_annual_reporting_v2_SQL.py

--- reporting/Maanluth_annual_reporting_v2_SQL.py
+++ reporting/Maanluth_annual_reporting_v2_SQL.py
@@ -11,16 +11,20 @@
 #
 # Author:      Moez Labiadh - FCBC, Nanaimo
 #
-# Created:     12-08-2022
+# Created:     23-01-2023
 # Updated:
 #-------------------------------------------------------------------------------
+
+
+import warnings
+warnings.simplefilter(action='ignore')
 
 import os
 import datetime
 import cx_Oracle
 import pandas as pd
 import geopandas as gpd
-from shapely import wkt
+#from shapely import wkt
 import fiona
 import numpy as np
 
@@ -90,9 +94,10 @@ def load_queries ():
     sql = {}
     sql['maan'] = """
                 SELECT
-               ipr.INTRID_SID, ipr.CROWN_LANDS_FILE, pip.CONTACT_ORGANIZATION_NAME,
+               ipr.INTRID_SID, ipr.CROWN_LANDS_FILE,
                ROUND (ipr.TENURE_AREA_IN_HECTARES, 2) TENURE_HECTARE, 
-               ROUND(SDO_GEOM.SDO_AREA(SDO_GEOM.SDO_INTERSECTION(pip.SHAPE,ipr.SHAPE, 0.005), 0.005, 'unit=HECTARE'), 2) OVERLAP_HECTARE
+               ROUND(SDO_GEOM.SDO_AREA(SDO_GEOM.SDO_INTERSECTION(pip.SHAPE,ipr.SHAPE, 0.005), 0.005, 'unit=HECTARE'), 2) OVERLAP_HECTARE,
+               SDO_UTIL.TO_WKTGEOMETRY(ipr.SHAPE) SHAPE
             
             FROM
                 WHSE_TANTALIS.TA_CROWN_TENURES_SVW ipr,
@@ -154,12 +159,47 @@ def read_query(connection,query):
             cursor.close()
 
 
-def get_maan_tenures (df_off,connection, sql):
+def esri_to_gdf (aoi):
+    """Returns a Geopandas file (gdf) based on 
+       an ESRI format vector (shp or featureclass/gdb)"""
+    
+    if '.shp' in aoi: 
+        gdf = gpd.read_file(aoi)
+    
+    elif '.gdb' in aoi:
+        l = aoi.split ('.gdb')
+        gdb = l[0] + '.gdb'
+        fc = os.path.basename(aoi)
+        gdf = gpd.read_file(filename= gdb, layer= fc)
+        
+    else:
+        raise Exception ('Format not recognized. Please provide a shp or featureclass (gdb)!')
+    
+    return gdf
+
+
+def df_2_gdf (df, crs):
+    """ Return a geopandas gdf based on a df with Geometry column"""
+    df['SHAPE'] = df['SHAPE'].astype(str)
+    df['geometry'] = gpd.GeoSeries.from_wkt(df['SHAPE'])
+    gdf = gpd.GeoDataFrame(df, geometry='geometry')
+    #df['geometry'] = df['SHAPE'].apply(wkt.loads)
+    #gdf = gpd.GeoDataFrame(df, geometry = df['geometry'])
+    gdf.crs = "EPSG:" + str(crs)
+    del df['SHAPE']
+    
+    return gdf
+
+
+def get_maan_tenures (df_off,connection,sql):
     """Returns a df containing Tenures offered within Maanulth Territory"""
         
     s_off = ",".join("'" + str(x) + "'" for x in df_off['FILE NUMBER'].tolist())
     query = sql['maan'].format(t= s_off)
     df_maan = read_query(connection,query)
+    
+    gdf_maan = df_2_gdf (df_maan, 3005)
+    gdf_maan.drop(['OVERLAP_HECTARE'], axis=1, inplace=True)
     
     df_maan = df_maan.groupby('CROWN_LANDS_FILE')[['TENURE_HECTARE', 'OVERLAP_HECTARE']].apply(sum).reset_index()
     
@@ -171,7 +211,7 @@ def get_maan_tenures (df_off,connection, sql):
     
     df_maan.sort_values(by = ['FILE NUMBER'], inplace = True)
     
-    return df_maan
+    return df_maan, gdf_maan
 
 
 def get_iha_overlaps (df_maan,connection, sql):
@@ -183,6 +223,7 @@ def get_iha_overlaps (df_maan,connection, sql):
     
     return df_iha
  
+    
 def get_lu_overlaps (df_maan,connection, sql):
     """"Return a df containing overlaps of Tenures and Land Use Units"""
     s_maan= ",".join("'" + str(x) + "'" for x in df_maan['FILE NUMBER'].tolist())
@@ -196,30 +237,9 @@ def get_lu_overlaps (df_maan,connection, sql):
     return df_lu, df_lu_sum
 
 
-def generate_shp(df_maan, connection, workspace, year):
+def generate_spatial_files(gdf, workspace, year):
     """Generate a Shapefile of Tenures"""
-    s_maan= ",".join("'" + str(x) + "'" for x in df_maan['FILE NUMBER'].tolist())
-    
-    q = """
-       SELECT
-       ipr.INTRID_SID, ipr.CROWN_LANDS_FILE,
-       ROUND (ipr.TENURE_AREA_IN_HECTARES, 2) AREA_HECTARE, 
-       SDO_UTIL.TO_WKTGEOMETRY(ipr.SHAPE) SHAPE
-       
-       FROM
-       WHSE_TANTALIS.TA_CROWN_TENURES_SVW ipr
-    
-       WHERE ipr.TENURE_STAGE = 'TENURE' 
-       AND ipr.CROWN_LANDS_FILE in ({t})
-        """
-    query = q.format(t= s_maan)
-    df = read_query(connection,query)
-    df['SHAPE'] = df['SHAPE'].astype(str)
-    df['geometry'] = df['SHAPE'].apply(wkt.loads)
-    
-    gdf = gpd.GeoDataFrame(df, geometry = df['geometry'])
-    gdf.crs = "EPSG:3005"
-    del gdf['SHAPE']
+
     
     shp_name = os.path.join(workspace, 'maan_report_{}_shapes.shp'.format(str(year)))
     kml_name = os.path.join(workspace, 'maan_report_{}_shapes.kml'.format(str(year)))
@@ -262,7 +282,7 @@ def generate_report (workspace, df_list, sheet_list,filename):
 def main():
     """Runs the program"""
     
-    workspace = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20220719_maanulth_annual_report_2022\20220812_2'
+    workspace = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20220719_maanulth_annual_report_2022\tests_2023'
     titan_report = os.path.join(workspace, 'TITAN_RPT009.xlsx')
     
     print ('Connecting to BCGW...')
@@ -282,20 +302,29 @@ def main():
     sql = load_queries ()
     
     print ("SQL-1: Getting Tenures within Maanulth Territory...")
-    df_maan = get_maan_tenures (df_off,connection, sql)
-
+    df_maan, gdf_maan = get_maan_tenures (df_off,connection, sql)
+    
+    
     print ("SQL-2: Getting overlaps with Important Harvest Areas...")
     df_iha = get_iha_overlaps (df_maan,connection, sql)
-
+    
     print ("SQL-3: Getting overlaps with Landscape Units...")
     df_lu, df_lu_sum = get_lu_overlaps (df_maan,connection, sql) 
-
+    
+    print ("Getting overlaps with Individual Nations")
+    indNat_fc = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\DATASETS\Maa-nulth.gdb\PreTreatyFirstNationAreas'
+    gdf_nat = esri_to_gdf (indNat_fc)
+    gdf_intr = gpd.overlay(gdf_maan, gdf_nat, how='intersection')
+    gdf_intr['OVERLAP_AREA'] = round(gdf_intr.area/10**4,2)
+    df_nat = pd.DataFrame(gdf_intr)
+    df_nat=df_nat[['INTRID_SID', 'CROWN_LANDS_FILE', 'FN_area', 'TENURE_HECTARE','OVERLAP_AREA']]
+    
     print ('Generating Outputs...')
-    df_list = [df_maan,df_iha,df_lu, df_lu_sum]
-    sheet_list = ['Offered Tenures in Maanulth', 'Overlay - IHA','Overlay - LU', 'LU Area Summary']
+    df_list = [df_maan,df_iha, df_nat, df_lu, df_lu_sum]
+    sheet_list = ['Offered Tenures in Maanulth', 'Overlay - IHA', 'Overlay - Nations','Overlay - LU', 'LU Area Summary']
     filename = 'Maanulth_annualReporting_{}_tables'.format(str(year))
     generate_report (workspace, df_list, sheet_list,filename)
     
-    generate_shp(df_maan, connection, workspace, year)
+    generate_spatial_files(gdf_maan, workspace, year)
     
 main ()
