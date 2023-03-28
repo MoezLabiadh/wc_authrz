a1adb2c3289d30e402c838d6f6554bd66968192f
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2023-03-28 15:13:18 -0700

    Create fix_proj.py

---
+++ STATUSING/generate html maps/fix_proj.py
@@ -0,0 +1,3 @@
+import os
+os.environ['PROJ_LIB'] = r'P:\corp\python3711\Library\share\proj' #this points gpd to the gdal proj lib. Missing from gpd conda install
+os.environ['GDAL_DATA'] = r'P:\corp\python3711\Library\share' #this points gpd to the gdal proj lib. Missing from gpd conda install
