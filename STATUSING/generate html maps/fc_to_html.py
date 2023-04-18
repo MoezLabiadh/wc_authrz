8f0cd004780964b497c2a81ad0eef6856bbe2647
geo-moe <60794110+MoezLabiadh@users.noreply.github.com>
2023-04-18 13:56:13 -0700

    Update fc_to_html.py

--- STATUSING/generate html maps/fc_to_html.py
+++ STATUSING/generate html maps/fc_to_html.py
@@ -1,13 +1,22 @@
 '''
 This script loops through the Feature classes stored
 in the one_status_common_datasets geodatabase 
-and export a HTML map for each layer
+and export a HTML maps
 '''
 
 import timeit
 import os
 import sys
 #import arcpy
+import geopandas as gpd
+import numpy as np
+import fiona
+import folium
+from folium.plugins import MeasureControl
+from folium.plugins import MousePosition
+from folium.features import LatLngPopup
+
+
 
 def add_proj_lib ():
     """
@@ -21,16 +30,49 @@ def add_proj_lib ():
         pass
 
 
+
+def create_map_template():
+    """Returns a folium map object"""
+    
+    map_obj = folium.Map()
+    
+    # Add the GeoBC basemap to the map
+    wms_url = 'https://maps.gov.bc.ca/arcgis/rest/services/province/web_mercator_cache/MapServer/tile/{z}/{y}/{x}'
+    wms_attribution = 'GeoBC, DataBC, TomTom, © OpenStreetMap Contributors'
+    folium.TileLayer(
+        tiles=wms_url,
+        name='GeoBC Basemap',
+        attr=wms_attribution,
+        overlay=False,
+        control=True,
+        transparent=True).add_to(map_obj)
+    
+    # Add a satellite basemap to the map
+    satellite_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+    satellite_attribution = 'Tiles &copy; Esri'
+    folium.TileLayer(
+        tiles=satellite_url,
+        name='Imagery Basemap',
+        attr=satellite_attribution,
+        overlay=False,
+        control=True).add_to(map_obj)
+    
+    # Add measure controls to the map
+    map_obj.add_child(MeasureControl())
+    
+    # Add mouse psotion to the map
+    MousePosition().add_to(map_obj)
+    
+    # Add the LatLngPopup plugin to the map
+    map_obj.add_child(LatLngPopup())
+    
+    return map_obj
+
+
+
 def generate_html_maps(status_gdb):
     """Creates a HTML map for each feature class in gdb"""
-    # Import required libraries
-    import geopandas as gpd
-    import numpy as np
-    import fiona
-    import folium
-    from folium.plugins import MeasureControl
-    from folium.plugins import MousePosition
-    from folium.features import LatLngPopup
+    
     # List all feature classes
     # Can replace with arcpy.ListFeatureClasses(). Fiona is faster!
     fc_list = fiona.listlayers(status_gdb)
@@ -39,7 +81,8 @@ def generate_html_maps(status_gdb):
     
     # Read the AOI feature class into a gdf 
     gdf_aoi = gpd.read_file(filename= status_gdb, layer= 'aoi')
-        
+    
+
     # Remove the aoi and aoi buffers from the list of feature classes
     fc_list = [x for x in fc_list if 'aoi' not in x]
     
@@ -67,36 +110,6 @@ def generate_html_maps(status_gdb):
                 gdf_fc[col] = gdf_fc[col].astype(str)
                 gdf_fc[col] = gdf_fc[col].str.wrap(width=20).str.replace('\n','<br>')
             
-            # Initiate a Map object and set extent based on the layer 
-            map_obj = folium.Map()
-            xmin,ymin,xmax,ymax = gdf_fc.to_crs(4326)['geometry'].total_bounds
-            map_obj.fit_bounds([[ymin, xmin], [ymax, xmax]])
-            
-            # Add the GeoBC basemap to the map
-            wms_url = 'https://maps.gov.bc.ca/arcgis/rest/services/province/web_mercator_cache/MapServer/tile/{z}/{y}/{x}'
-            wms_attribution = 'GeoBC, DataBC, TomTom, © OpenStreetMap Contributors'
-            folium.TileLayer(
-                tiles=wms_url,
-                name='GeoBC Basemap',
-                attr=wms_attribution,
-                overlay=False,
-                control=True,
-                transparent=True).add_to(map_obj)
-
-            # Add a satellite basemap to the map
-            satellite_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-            satellite_attribution = 'Tiles &copy; Esri'
-            folium.TileLayer(
-                tiles=satellite_url,
-                name='Imagery Basemap',
-                attr=satellite_attribution,
-                overlay=False,
-                control=True).add_to(map_obj)
-                    
-            # Add the AOI layer to the folium map
-            folium.GeoJson(data=gdf_aoi, name='AOI',
-                           style_function=lambda x:{'color': 'red',
-                                                    'weight': 3}).add_to(map_obj)
             
             # Assign random colors to the features (for legend)
             for x in gdf_fc.index:
@@ -105,29 +118,33 @@ def generate_html_maps(status_gdb):
                 color = '#'+''.join(color).upper()
                 gdf_fc.at[x, 'color'] = color
                 
+            # Create an empty map
+            map_one = create_map_template()
+            
+            # Add the AOI layer to the folium map
+            lyr_aoi = folium.GeoJson(data=gdf_aoi, name='AOI',
+                                     style_function=lambda x:{'color': 'red',
+                                                              'weight': 3})
+            
+            lyr_aoi.add_to(map_one)
+            
+            # Zoom the map to the layer extent
+            xmin,ymin,xmax,ymax = gdf_fc.to_crs(4326)['geometry'].total_bounds
+            map_one.fit_bounds([[ymin, xmin], [ymax, xmax]])
+                
             # Add the main layer to the folium map
-            folium.GeoJson(data=gdf_fc,
-                           name=fc,
-                           style_function= lambda x: {'fillColor': x['properties']['color'],
-                                                      'color': x['properties']['color'],
-                                                      'weight': 2},
-                           tooltip=folium.features.GeoJsonTooltip(fields=[label_col], 
-                                                                  labels=True),
-                           popup=folium.features.GeoJsonPopup(fields=popup_cols, 
-                                                              sticky=False,
-                                                              max_width=380)).add_to(map_obj)
+            lyr_fc = folium.GeoJson(data=gdf_fc,
+                                    name=fc,
+                                    style_function= lambda x: {'fillColor': x['properties']['color'],
+                                                               'color': x['properties']['color'],
+                                                               'weight': 2},
+                                    tooltip=folium.features.GeoJsonTooltip(fields=[label_col], 
+                                                                           labels=True),
+                              popup=folium.features.GeoJsonPopup(fields=popup_cols, 
+                                                                 sticky=False,
+                                                                 max_width=380))
             
-            # add layer controls to the map
-            folium.LayerControl().add_to(map_obj)
-            
-            # add measure controls to the map
-            map_obj.add_child(MeasureControl())
-            
-            # add mouse psotion to the map
-            MousePosition().add_to(map_obj)
-            
-            # add the LatLngPopup plugin to the map
-            map_obj.add_child(LatLngPopup())
+            lyr_fc.add_to(map_one)
             
             # Create a Legend
             #legend colors and names
@@ -163,14 +180,17 @@ def generate_html_maps(status_gdb):
             legend_html += '</div>'
             
             #add the legend to the map
-            map_obj.get_root().html.add_child(folium.Element(legend_html))
+            map_one.get_root().html.add_child(folium.Element(legend_html))
             
+            # add layer controls to the map
+            folium.LayerControl().add_to(map_one)
         
             # Save the interavtive map to html file
             out_loc = r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\TOOLS\SCRIPTS\STATUSING\fc_to_html\maps'
-            map_obj.save(os.path.join(out_loc, fc+'.html'))
+            map_one.save(os.path.join(out_loc, fc+'.html'))
             
         counter += 1
+
 
 
 if __name__==__name__:
@@ -182,6 +202,7 @@ if __name__==__name__:
     # This is an example of a one_status_common_datasets geodatabase
     status_gdb = r'\\spatialfiles.bcgov\work\lwbc\visr\Workarea\FCBC_VISR\Lands_Statusing\1414630\one_status_common_datasets_aoi.gdb'
     
+
     generate_html_maps(status_gdb)
     
     finish_t = timeit.default_timer() #finish time
