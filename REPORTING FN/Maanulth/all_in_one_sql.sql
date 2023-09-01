6f4c8d2f8468bd0e1f6668665837863a21f3de8f
geo-moe <60794110+MoezLabiadh@users.noreply.github.com>
2023-09-01 15:25:13 -0700

    Create all_in_one_sql.sql

---
+++ REPORTING FN/Maanulth/all_in_one_sql.sql
@@ -0,0 +1,104 @@
+                SELECT --TN.INTRID_SID, 
+                       --TN.DISPOSITION_TRANSACTION_SID,
+                       CASE
+                        WHEN iha.TREATY_SIDE_AGREEMENT_AREA_ID IS NOT NULL
+                          THEN 'YES'
+                            ELSE 'NO'
+                              END AS OVERLAP_IHA,
+                        
+                       iha.TREATY_SIDE_AGREEMENT_AREA_ID,
+                       TN.FILE_NBR,
+                       TN.STAGE,
+                       TN.STATUS,
+                       TN.APPLICATION_TYPE,
+                       --TN.EFFECTIVE_DATE,
+                       TN.TENURE_TYPE,
+                       TN.TENURE_SUBTYPE,
+                       TN.TENURE_PURPOSE,
+                       TN.TENURE_SUBPURPOSE,
+                       TN.TENURE_PURPOSE || ' ' || '-' || ' ' || TN.TENURE_SUBPURPOSE AS FULL_PURPOSE,
+                       TF.OFFERED_DATE, 
+                       TN.EXPIRY_DATE,
+                       (EXTRACT(YEAR FROM TN.EXPIRY_DATE) - EXTRACT(YEAR FROM TF.OFFERED_DATE)) AS TENURE_LENGTH_YRS,
+                       ROUND(TN.AREA_HA,2) AS AREA_HA,
+                       
+                       SDO_UTIL.TO_WKTGEOMETRY(TN.SHAPE) SHAPE
+                       --TN.LOCATION_DSC,
+                       --TN.CLIENT_NAME_PRIMARY
+                      
+                FROM(
+                SELECT
+                      CAST(IP.INTRID_SID AS NUMBER) INTRID_SID,
+                      CAST(DT.DISPOSITION_TRANSACTION_SID AS NUMBER) DISPOSITION_TRANSACTION_SID,
+                      DS.FILE_CHR AS FILE_NBR,
+                      SG.STAGE_NME AS STAGE,
+                      TT.STATUS_NME AS STATUS,
+                      DT.APPLICATION_TYPE_CDE AS APPLICATION_TYPE,
+                      TS.EFFECTIVE_DAT AS EFFECTIVE_DATE,
+                      TY.TYPE_NME AS TENURE_TYPE,
+                      ST.SUBTYPE_NME AS TENURE_SUBTYPE,
+                      PU.PURPOSE_NME AS TENURE_PURPOSE,
+                      SP.SUBPURPOSE_NME AS TENURE_SUBPURPOSE,
+                      DT.COMMENCEMENT_DAT AS COMMENCEMENT_DATE,
+                      DT.EXPIRY_DAT AS EXPIRY_DATE,
+                      IP.AREA_CALC_CDE,
+                      IP.AREA_HA_NUM AS AREA_HA,
+                      DT.LOCATION_DSC,
+                      OU.UNIT_NAME,
+                      --IP.LEGAL_DSC,
+                      CONCAT(PR.LEGAL_NAME, PR.FIRST_NAME || ' ' || PR.LAST_NAME) AS CLIENT_NAME_PRIMARY,
+                      SP.SHAPE
+                      
+                FROM WHSE_TANTALIS.TA_DISPOSITION_TRANSACTIONS DT 
+                  JOIN WHSE_TANTALIS.TA_INTEREST_PARCELS IP 
+                    ON DT.DISPOSITION_TRANSACTION_SID = IP.DISPOSITION_TRANSACTION_SID
+                      AND IP.EXPIRY_DAT IS NULL
+                  JOIN WHSE_TANTALIS.TA_DISP_TRANS_STATUSES TS
+                    ON DT.DISPOSITION_TRANSACTION_SID = TS.DISPOSITION_TRANSACTION_SID 
+                      AND TS.EXPIRY_DAT IS NULL
+                  JOIN WHSE_TANTALIS.TA_DISPOSITIONS DS
+                    ON DS.DISPOSITION_SID = DT.DISPOSITION_SID
+                  JOIN WHSE_TANTALIS.TA_STAGES SG 
+                    ON SG.CODE_CHR = TS.CODE_CHR_STAGE
+                  JOIN WHSE_TANTALIS.TA_STATUS TT 
+                    ON TT.CODE_CHR = TS.CODE_CHR_STATUS
+                  JOIN WHSE_TANTALIS.TA_AVAILABLE_TYPES TY 
+                    ON TY.TYPE_SID = DT.TYPE_SID    
+                  JOIN WHSE_TANTALIS.TA_AVAILABLE_SUBTYPES ST 
+                    ON ST.SUBTYPE_SID = DT.SUBTYPE_SID 
+                      AND ST.TYPE_SID = DT.TYPE_SID 
+                  JOIN WHSE_TANTALIS.TA_AVAILABLE_PURPOSES PU 
+                    ON PU.PURPOSE_SID = DT.PURPOSE_SID    
+                  JOIN WHSE_TANTALIS.TA_AVAILABLE_SUBPURPOSES SP 
+                    ON SP.SUBPURPOSE_SID = DT.SUBPURPOSE_SID 
+                      AND SP.PURPOSE_SID = DT.PURPOSE_SID 
+                  JOIN WHSE_TANTALIS.TA_ORGANIZATION_UNITS OU 
+                    ON OU.ORG_UNIT_SID = DT.ORG_UNIT_SID 
+                  JOIN WHSE_TANTALIS.TA_TENANTS TE 
+                    ON TE.DISPOSITION_TRANSACTION_SID = DT.DISPOSITION_TRANSACTION_SID
+                      AND TE.SEPARATION_DAT IS NULL
+                      AND TE.PRIMARY_CONTACT_YRN = 'Y'
+                  JOIN WHSE_TANTALIS.TA_INTERESTED_PARTIES PR
+                    ON PR.INTERESTED_PARTY_SID = TE.INTERESTED_PARTY_SID
+                  JOIN WHSE_TANTALIS.TA_INTEREST_PARCEL_SHAPES SP
+                    ON SP.INTRID_SID = IP.INTRID_SID
+                    
+                  JOIN WHSE_ADMIN_BOUNDARIES.PIP_CONSULTATION_AREAS_SP pip
+                    ON SDO_RELATE (SP.SHAPE, pip.SHAPE, 'mask=ANYINTERACT') = 'TRUE'
+                      AND pip.CONTACT_ORGANIZATION_NAME = q'[Maa-nulth First Nations]'
+                       
+                WHERE TT.STATUS_NME IN ('DISPOSITION IN GOOD STANDING', 'OFFERED', 'OFFER ACCEPTED')
+                
+                ORDER BY TS.EFFECTIVE_DAT DESC) TN
+                
+                JOIN (SELECT DISPOSITION_TRANSACTION_SID, EFFECTIVE_DAT AS OFFERED_DATE 
+                      FROM WHSE_TANTALIS.TA_DISP_TRANS_STATUSES
+                      WHERE CODE_CHR_STATUS = 'OF'
+                      AND EFFECTIVE_DAT BETWEEN TO_DATE('01/09/2022', 'DD/MM/YYYY') AND TO_DATE('31/08/2023', 'DD/MM/YYYY')) TF
+                      
+                     ON TF.DISPOSITION_TRANSACTION_SID = TN.DISPOSITION_TRANSACTION_SID
+                     
+                LEFT JOIN WHSE_LEGAL_ADMIN_BOUNDARIES.FNT_TREATY_SIDE_AGREEMENTS_SP iha
+                    ON SDO_RELATE (iha.GEOMETRY, TN.SHAPE, 'mask=ANYINTERACT') = 'TRUE'
+                       AND iha.AREA_TYPE = 'Important Harvest Area'
+                       AND iha.STATUS = 'ACTIVE';
