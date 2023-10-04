4f8d49f3dc2f028e9d604b3a85c8ef9de8abea4b
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2023-10-04 09:41:50 -0700

    Update changeEnv_laucnhPro.bat

--- STATUSING/proswap/changeEnv_laucnhPro.bat
+++ STATUSING/proswap/changeEnv_laucnhPro.bat
@@ -1,32 +1,33 @@
 @echo off
-rem This script changes the Python environment of ArcGIS Pro to P:/whse_np/corp/python3916
-rem and then launches ArcGIS Pro application using the proswap command.
+rem This bat file uses proswap.bat to change the python environment of arcgis pro
+rem proswap.bat is located in E:\XXX\ArcGIS\Pro\bin\Python\Scripts
+rem The new python environment is P:\XXX\python3916
 
-rem Set the path to the new Python environment
-set PYTHONPATH=P:\XXXXXXXXXXXXXX\python3916
+rem Change the current directory to the location of proswap.bat
+cd /d E:\XXX\ArcGIS\Pro\bin\Python\Scripts
 
-rem Check if the new Python environment exists
-if not exist %PYTHONPATH% (
-  echo The new Python environment does not exist. Please check the path and try again.
-  rem Pause the screen for 10 seconds
-  timeout /t 10 /nobreak >nul
-  exit /b 1
+rem Check the current python environment and compare it with the new one
+rem call python -m sys -c "print(sys.prefix)"
+call python -c "import sys; print(sys.prefix)"
+set current_env=%errorlevel%
+if %current_env% == P:\corp\python3916 (
+    echo Python environment is already set to P:\corp\python3916.
+) else (
+    rem Call proswap.bat with the new python environment as an argument
+    echo Changing Python environment to P:\XXX\python3916.
+    call proswap.bat P:\XXX\python3916
+
+    rem Check the status of the operation
+    if %errorlevel% == 0 (
+        echo Python environment changed successfully.
+    ) else (
+        echo An error occurred while changing the python environment.
+    )
 )
 
-rem Set the path to the ArcGIS Pro executable
-set ARCPROPATH="E:\XXXXXXXXXXXXXXXXXX\ArcGIS\Pro\bin\ArcGISPro.exe"
+rem Launch arcgis pro using the full path to the executable
+echo Launching arcgis pro
+start "" "E:\XXX\ArcGIS\Pro\bin\ArcGISPro.exe"
 
-rem Check if the ArcGIS Pro executable exists
-if not exist %ARCPROPATH% (
-  echo The ArcGIS Pro executable does not exist. Please check the path and try again.
-  rem Pause the screen for 10 seconds
-  timeout /t 10 /nobreak >nul
-  exit /b 2
-)
-
-rem Activate the new Python environment for the current and all future sessions of ArcGIS Pro using the proswap command[^1^][5]
-proswap %PYTHONPATH%
-
-rem Launch ArcGIS Pro with the new Python environment
-start "" %ARCPROPATH%
-
+rem Pause the execution and wait for user input
+pause
