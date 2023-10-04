fe72641fbaf233b963b707cea58a997f2dec84d7
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2023-10-04 11:56:44 -0700

    Update changeEnv_laucnhPro.bat

--- STATUSING/proswap/changeEnv_laucnhPro.bat
+++ STATUSING/proswap/changeEnv_laucnhPro.bat
@@ -1,32 +1,30 @@
 @echo off
-rem This bat file uses proswap.bat to change the python environment of arcgis pro
+rem This bat file changes the python environment of ArcGIS Pro, then launches the app.
 rem proswap.bat is located in E:\XXX\ArcGIS\Pro\bin\Python\Scripts
 rem The new python environment is P:\XXX\python3916
 
-rem Change the current directory to the location of proswap.bat
-cd /d E:\XXX\ArcGIS\Pro\bin\Python\Scripts
+rem Check the current python environment using Python itself
+for /f "delims=" %%a in ('python -c "import sys; print(sys.prefix)"') do set current_env=%%a
 
-rem Check the current python environment and compare it with the new one
-rem call python -m sys -c "print(sys.prefix)"
-call python -c "import sys; print(sys.prefix)"
-set current_env=%errorlevel%
-if %current_env% == P:\corp\python3916 (
+if "%current_env%"=="P:\XXX\python3916" (
     echo Python environment is already set to P:\corp\python3916.
 ) else (
     rem Call proswap.bat with the new python environment as an argument
     echo Changing Python environment to P:\XXX\python3916.
+
+    cd /d E:\sw_nt\ArcGIS\Pro\bin\Python\Scripts
     call proswap.bat P:\XXX\python3916
 
     rem Check the status of the operation
-    if %errorlevel% == 0 (
+    if %errorlevel%==0 (
         echo Python environment changed successfully.
     ) else (
         echo An error occurred while changing the python environment.
     )
 )
 
-rem Launch arcgis pro using the full path to the executable
-echo Launching arcgis pro
+rem Launch ArcGIS Pro using the full path to the executable
+echo Launching ArcGIS Pro
 start "" "E:\XXX\ArcGIS\Pro\bin\ArcGISPro.exe"
 
 rem Pause the execution and wait for user input
