fbce6b2371917ceb4b015e235a82a8232c25b832
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2023-10-27 15:18:20 -0700

    hmn killer whale script

--- REPORTING FN/HMN/hmn_privateMoorage_killerWhale_applics.py
+++ REPORTING FN/HMN/hmn_privateMoorage_killerWhale_applics.py
@@ -4,51 +4,153 @@ warnings.simplefilter(action='ignore')
 import os
 import cx_Oracle
 import pandas as pd
+import geopandas as gpd
+from shapely import wkb
 
 
 def connect_to_DB (username,password,hostname):
     """ Returns a connection to Oracle database"""
     try:
         connection = cx_Oracle.connect(username, password, hostname, encoding="UTF-8")
+        cursor = connection.cursor()
         print  ("...Successffuly connected to the database")
     except:
         raise Exception('...Connection failed! Please verifiy your login parameters')
 
-    return connection
+    return connection, cursor
+
+
+def esri_to_gdf (aoi):
+    """Returns a Geopandas file (gdf) based on 
+       an ESRI format vector (shp or featureclass/gdb)"""
+    
+    if '.shp' in aoi: 
+        gdf = gpd.read_file(aoi)
+    
+    elif '.gdb' in aoi:
+        l = aoi.split ('.gdb')
+        gdb = l[0] + '.gdb'
+        fc = os.path.basename(aoi)
+        gdf = gpd.read_file(filename= gdb, layer= fc)
+        
+    else:
+        raise Exception ('Format not recognized. Please provide a shp or featureclass (gdb)!')
+    
+    return gdf
+
+
+
+def get_wkb(gdf):
+    """Returns WKB object from gdf"""
+    
+    geom = gdf['geometry'].iloc[0]
+    
+    # Check if the geometry is a MultiPolygon
+    if geom.geom_type == 'MultiPolygon':
+        wkb_aoi = wkb.dumps(geom, output_dimension=2)
+    else:
+        wkb_aoi = geom.wkb
+
+    return wkb_aoi
+
+
+def read_query(connection,cursor,query,bvars):
+    "Returns a df containing SQL Query results"
+    cursor.execute(query, bvars)
+    names = [x[0] for x in cursor.description]
+    rows = cursor.fetchall()
+    df = pd.DataFrame(rows, columns=names)
+    
+    return df  
+
 
 
 def load_queries():
     """ Return the SQL queries that will be executed"""
     sql= {}
     
-    sql['cmb']= """
-                SELECT
-                  --wlw.FWA_WATERSHED_GROUP_CODE,
+    sql['new']= """
+            SELECT* FROM(
+            SELECT
                   --CAST(IP.INTRID_SID AS NUMBER) INTEREST_PARCEL_ID,
                   --CAST(DT.DISPOSITION_TRANSACTION_SID AS NUMBER) DISPOSITION_TRANSACTION_ID,
                   DS.FILE_CHR AS FILE_NBR,
                   SG.STAGE_NME AS STAGE,
-                  --TT.ACTIVATION_CDE,
                   TT.STATUS_NME AS STATUS,
                   DT.APPLICATION_TYPE_CDE AS APPLICATION_TYPE,
-                  --TS.EFFECTIVE_DAT AS EFFECTIVE_DATE,
-                  DT.RECEIVED_DAT AS RECEIVED_DATE,
-                  EXTRACT(YEAR FROM DT.RECEIVED_DAT) AS RECEIVED_YEAR,
+                  TS.EFFECTIVE_DAT AS EFFECTIVE_DATE,
                   TY.TYPE_NME AS TENURE_TYPE,
                   ST.SUBTYPE_NME AS TENURE_SUBTYPE,
                   PU.PURPOSE_NME AS TENURE_PURPOSE,
                   SP.SUBPURPOSE_NME AS TENURE_SUBPURPOSE,
-                  --DT.DOCUMENT_CHR,
-                  --DT.ENTERED_DAT AS ENTERED_DATE,
-                  --DT.COMMENCEMENT_DAT AS COMMENCEMENT_DATE,
-                  --DT.EXPIRY_DAT AS EXPIRY_DATE,
-                  --IP.AREA_CALC_CDE,
-                  ROUND(IP.AREA_HA_NUM,2) AS AREA_HA,
-                  DT.LOCATION_DSC
-                  --OU.UNIT_NAME
-                  --IP.LEGAL_DSC,
-                  --CONCAT(PR.LEGAL_NAME, PR.FIRST_NAME || ' ' || PR.LAST_NAME) AS CLIENT_NAME_PRIMARY
-                  --SP.SHAPE
+                  DT.LOCATION_DSC,
+                  SH.SHAPE
+
+            FROM WHSE_TANTALIS.TA_DISPOSITION_TRANSACTIONS DT 
+              JOIN WHSE_TANTALIS.TA_INTEREST_PARCELS IP 
+                ON DT.DISPOSITION_TRANSACTION_SID = IP.DISPOSITION_TRANSACTION_SID
+                  AND IP.EXPIRY_DAT IS NULL
+              JOIN WHSE_TANTALIS.TA_DISP_TRANS_STATUSES TS
+                ON DT.DISPOSITION_TRANSACTION_SID = TS.DISPOSITION_TRANSACTION_SID 
+                  AND TS.EXPIRY_DAT IS NULL
+              JOIN WHSE_TANTALIS.TA_DISPOSITIONS DS
+                ON DS.DISPOSITION_SID = DT.DISPOSITION_SID
+              JOIN WHSE_TANTALIS.TA_STAGES SG 
+                ON SG.CODE_CHR = TS.CODE_CHR_STAGE
+              JOIN WHSE_TANTALIS.TA_STATUS TT 
+                ON TT.CODE_CHR = TS.CODE_CHR_STATUS
+              JOIN WHSE_TANTALIS.TA_AVAILABLE_TYPES TY 
+                ON TY.TYPE_SID = DT.TYPE_SID    
+              JOIN WHSE_TANTALIS.TA_AVAILABLE_SUBTYPES ST 
+                ON ST.SUBTYPE_SID = DT.SUBTYPE_SID 
+                  AND ST.TYPE_SID = DT.TYPE_SID 
+              JOIN WHSE_TANTALIS.TA_AVAILABLE_PURPOSES PU 
+                ON PU.PURPOSE_SID = DT.PURPOSE_SID    
+              JOIN WHSE_TANTALIS.TA_AVAILABLE_SUBPURPOSES SP 
+                ON SP.SUBPURPOSE_SID = DT.SUBPURPOSE_SID 
+                  AND SP.PURPOSE_SID = DT.PURPOSE_SID 
+              JOIN WHSE_TANTALIS.TA_ORGANIZATION_UNITS OU 
+                ON OU.ORG_UNIT_SID = DT.ORG_UNIT_SID 
+              JOIN WHSE_TANTALIS.TA_TENANTS TE 
+                ON TE.DISPOSITION_TRANSACTION_SID = DT.DISPOSITION_TRANSACTION_SID
+                  AND TE.SEPARATION_DAT IS NULL
+                  AND TE.PRIMARY_CONTACT_YRN = 'Y'
+              JOIN WHSE_TANTALIS.TA_INTERESTED_PARTIES PR
+                ON PR.INTERESTED_PARTY_SID = TE.INTERESTED_PARTY_SID
+            	
+              JOIN WHSE_TANTALIS.TA_INTEREST_PARCEL_SHAPES SH
+                ON SH.INTRID_SID = IP.INTRID_SID
+                
+              JOIN WHSE_ADMIN_BOUNDARIES.PIP_CONSULTATION_AREAS_SP FN
+                ON SDO_RELATE (SH.SHAPE, FN.SHAPE, 'mask=ANYINTERACT') = 'TRUE'
+                  AND FN.CNSLTN_AREA_NAME = q'[Hul'qumi'num Nations - Core Territory]'
+                  AND FN.CONTACT_NAME = 'Cowichan Tribes' ) TN
+            
+            WHERE TN.STAGE = 'APPLICATION'
+              AND TN.STATUS = 'ACCEPTED'
+              AND TN.APPLICATION_TYPE = 'NEW' 
+              AND TN.TENURE_SUBPURPOSE = 'PRIVATE MOORAGE'
+              AND SDO_RELATE (TN.SHAPE, SDO_GEOMETRY(:wkb_skw, 3005),'mask=ANYINTERACT') = 'TRUE'
+             
+            ORDER BY TN.EFFECTIVE_DATE DESC
+    """
+    
+    sql['rep']= """
+            SELECT* FROM(
+            SELECT
+                  --CAST(IP.INTRID_SID AS NUMBER) INTEREST_PARCEL_ID,
+                  --CAST(DT.DISPOSITION_TRANSACTION_SID AS NUMBER) DISPOSITION_TRANSACTION_ID,
+                  DS.FILE_CHR AS FILE_NBR,
+                  SG.STAGE_NME AS STAGE,
+                  TT.STATUS_NME AS STATUS,
+                  DT.APPLICATION_TYPE_CDE AS APPLICATION_TYPE,
+                  TS.EFFECTIVE_DAT AS EFFECTIVE_DATE,
+                  TY.TYPE_NME AS TENURE_TYPE,
+                  ST.SUBTYPE_NME AS TENURE_SUBTYPE,
+                  PU.PURPOSE_NME AS TENURE_PURPOSE,
+                  SP.SUBPURPOSE_NME AS TENURE_SUBPURPOSE,
+                  DT.LOCATION_DSC,
+                  SH.SHAPE
                   
             FROM WHSE_TANTALIS.TA_DISPOSITION_TRANSACTIONS DT 
               JOIN WHSE_TANTALIS.TA_INTEREST_PARCELS IP 
@@ -81,19 +183,30 @@ def load_queries():
                   AND TE.PRIMARY_CONTACT_YRN = 'Y'
               JOIN WHSE_TANTALIS.TA_INTERESTED_PARTIES PR
                 ON PR.INTERESTED_PARTY_SID = TE.INTERESTED_PARTY_SID
-              JOIN WHSE_TANTALIS.TA_INTEREST_PARCEL_SHAPES SP
-                ON SP.INTRID_SID = IP.INTRID_SID
+            	
+              JOIN WHSE_TANTALIS.TA_INTEREST_PARCEL_SHAPES SH
+                ON SH.INTRID_SID = IP.INTRID_SID
                 
-              JOIN WHSE_WATER_MANAGEMENT.WLS_WATER_LIC_WATERSHEDS_SP wlw
-                ON SDO_RELATE (SP.SHAPE, wlw.SHAPE, 'mask=ANYINTERACT') = 'TRUE'
-                  AND wlw.FWA_WATERSHED_GROUP_CODE = 'CAMB'  
+              JOIN WHSE_ADMIN_BOUNDARIES.PIP_CONSULTATION_AREAS_SP FN
+                ON SDO_RELATE (SH.SHAPE, FN.SHAPE, 'mask=ANYINTERACT') = 'TRUE'
+                  AND FN.CNSLTN_AREA_NAME = q'[Hul'qumi'num Nations - Core Territory]'
+                  AND FN.CONTACT_NAME = 'Cowichan Tribes') TN
             
+            WHERE TN.STAGE = 'APPLICATION'
+              AND TN.STATUS = 'ACCEPTED'
+              AND TN.APPLICATION_TYPE = 'REP' 
+              AND TN.TENURE_SUBPURPOSE = 'PRIVATE MOORAGE'
             
-            WHERE TT.ACTIVATION_CDE= 'ACT'
-              AND DT.RECEIVED_DAT > TO_DATE('01/01/2021', 'DD/MM/YYYY')
+              AND TN.FILE_NBR NOT IN (SELECT CROWN_LANDS_FILE 
+                                     FROM WHSE_TANTALIS.TA_CROWN_TENURES_SVW
+                                     WHERE TENURE_STATUS = 'DISPOSITION IN GOOD STANDING')
               
-            ORDER BY DT.RECEIVED_DAT DESC
-    """
+              AND SDO_RELATE (TN.SHAPE, SDO_GEOMETRY(:wkb_skw, 3005),'mask=ANYINTERACT') = 'TRUE' 
+                      
+            ORDER BY TN.EFFECTIVE_DATE DESC
+    """   
+    
+    
     return sql
 
 
@@ -131,16 +244,38 @@ if __name__==__name__:
     hostname = 'bcgw.bcgov/idwprod1.bcgov'
     bcgw_user = os.getenv('bcgw_user')
     bcgw_pwd = os.getenv('bcgw_pwd')
-    connection = connect_to_DB (bcgw_user,bcgw_pwd,hostname)
+    connection, cursor = connect_to_DB (bcgw_user,bcgw_pwd,hostname)
+    
+    print("\nReading SRKW dataset...")
+    shp_kw= r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\FCBC_VISR\Regional_Datasets\DFO\DFO_SARA_CritHab_2022_KillerWhale.shp'
+    gdf_kw= esri_to_gdf (shp_kw)
+    
+    gdf_skw= gdf_kw.loc[gdf_kw['Population']=='Northeast Pacific Southern Resident']
+    
+    wkb_skw= get_wkb (gdf_skw)
     
     print ("\nRunning SQL queries...")
     sql = load_queries ()
     
-    df= pd.read_sql(sql['cmb'], connection)
-    df['RECEIVED_DATE'] =  pd.to_datetime(df['RECEIVED_DATE'], infer_datetime_format=True, errors = 'coerce').dt.date
+    cursor.setinputsizes(wkb_skw=cx_Oracle.BLOB)
     
-    df.drop_duplicates(['FILE_NBR'], inplace= True)
+    bvars = {'wkb_skw': wkb_skw}
+    
+    df_new = read_query(connection, cursor, sql['new'], bvars)
+    df_new.drop('SHAPE', axis=1, inplace= True)
+    df_new['EFFECTIVE_DATE'] =  pd.to_datetime(df_new['EFFECTIVE_DATE'], infer_datetime_format=True, errors = 'coerce').dt.date
+    
+    cursor.setinputsizes(wkb_skw=cx_Oracle.BLOB)
+    
+    bvars = {'wkb_skw': wkb_skw}
+    
+    df_rep = read_query(connection, cursor, sql['rep'], bvars)
+    df_rep.drop('SHAPE', axis=1, inplace= True)
+    df_rep['EFFECTIVE_DATE'] =  pd.to_datetime(df_rep['EFFECTIVE_DATE'], infer_datetime_format=True, errors = 'coerce').dt.date
+    
+    #l= ",".join("'"+ str(x)+ "'" for x in df_new['FILE_NBR'].to_list())
+
     
     print ("\nExporting Results...")
-    workspace= r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20231024_campbellRiver_landApplics'
-    generate_report (workspace, [df], ['list'],'20231024_campbellRiv_landsApplics')
+    workspace= r'\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\WORKSPACE\20231027_hmn_srkw_privateMoorage'
+    generate_report (workspace, [df_new,df_rep], ['NEW Applics', 'REP Applics'],'20231027_privateMoorage_quwutsunCore_srkw')
