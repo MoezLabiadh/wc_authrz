347193aeca1a8c2e58a85976742d83541246a11f
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2024-01-22 15:59:28 -0800

    Update proswap_subprocess.py

--- STATUSING/proswap/proswap_subprocess.py
+++ STATUSING/proswap/proswap_subprocess.py
@@ -1,11 +1,17 @@
 import subprocess
 
-def run_proswap(env_name):
-    command = f"proswap {env_name}"
+def run_proswap(proswap_path, env_path):
+    """Runs proswap.bat to change Arcpro active python environment"""
+    command = f"{proswap_path} {env_path}"
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
 
     if process.returncode != 0:
         print(f"Error occurred: {error.decode('utf-8')}")
     else:
-        print(f"Output: {output.decode('utf-8')}")
+        print(f"SUCESS: {output.decode('utf-8')}")
+
+
+proswap_path= r'E:\sw_nt\ArcGIS\Pro\bin\Python\Scripts\proswap.bat'
+env_path= r'P:\corp\python_ast'
+run_proswap(proswap_path, env_path)
